3c6a73f79dc53055027e472f311f11c933970705
nfell2009 <nfell2009@live.co.uk>
2015-10-30 22:38:10 +0000

    Update example.php

--- example.php
+++ example.php
@@ -3,5 +3,5 @@ require_once('minepack.php');
 ?>
 
 A 16x16 size apple:
-<img src="<?php echo "$mp_apple_16"; ?>">
+<img src="<?php echo '$mp_apple_16'; ?>">
 <!-- this code should work. Well... it won't. Because there is no images to download yet-->
